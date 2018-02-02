d3f8ab43f1ea4d5004e261c519bcf7297186f6bb
Anwesh43 <anweshthecool0@gmail.com>
2018-02-02 07:59:19 +0530

    adding Animator class

--- textwithinlineview/src/main/java/ui/anwesome/com/textwithinlineview/TextWithinLineView.kt
+++ textwithinlineview/src/main/java/ui/anwesome/com/textwithinlineview/TextWithinLineView.kt
@@ -70,4 +70,29 @@ class TextWithinLineView(ctx:Context):View(ctx) {
             }
         }
     }
+    data class Animator(var view:View, var animated:Boolean = false){
+        fun animate(updatecb:()->Unit) {
+           if(animated) {
+               updatecb()
+               try {
+                   Thread.sleep(50)
+                   view.postInvalidate()
+               }
+               catch(ex:Exception) {
+
+               }
+           }
+        }
+        fun start() {
+            if(!animated) {
+                animated = true
+                view.postInvalidate()
+            }
+        }
+        fun stop() {
+            if(animated) {
+                animated = false
+            }
+        }
+    }
 }
